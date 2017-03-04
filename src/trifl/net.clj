a5dedeabd8e5e1a4e69c72cdf4adfe302d8847fe
Duncan McGreggor <oubiwann@gmail.com>
2017-03-04 12:59:26 -0600

    Added more net functions.

--- src/trifl/net.clj
+++ src/trifl/net.clj
@@ -1,4 +1,19 @@
 (ns trifl.net)
 
+(defn get-local-host
+  "Get the java.net.InetAddress object for the local host."
+  []
+  (java.net.InetAddress/getLocalHost))
+
 (defn get-local-ip []
-  (.getHostAddress (java.net.InetAddress/getLocalHost)))
+  "Get the IP address for the machine that this JVM is running on.
+
+  Uses the `java.net.InetAddress` method `getHostAddress`."
+  (.getHostAddress (get-local-host)))
+
+(defn get-local-hostname
+  "Get the hostname for the machine that this JVM is running on.
+
+  Uses the `java.net.InetAddress` method `getHostName`."
+  []
+  (.getHostName (get-local-host)))
