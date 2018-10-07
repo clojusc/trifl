b9bc9345f601f3ad56929d4b0d2b0c17210d5348
Duncan McGreggor <oubiwann@gmail.com>
2018-10-07 15:59:31 -0500

    Updated aliases.

--- project.clj
+++ project.clj
@@ -23,23 +23,32 @@
         [org.clojure/tools.namespace "0.2.11"]
         [org.clojure/data.xml "0.0.8"]]}}
   :aliases {
-    "check-deps" [
-      "with-profile" "+test" "ancient" "check" ":all"]
-    "kibit" [
-      "with-profile" "+test" "do"
-        ["shell" "echo" "== Kibit =="]
-        ["kibit"]]
-    "outlaw" [
-      "with-profile" "+test"
-      "eastwood" "{:namespaces [:source-paths] :source-paths [\"src\"]}"]
-    "lint" [
-      "with-profile" "+test" "do"
-        ["check"] ["kibit"] ["outlaw"]]
-    "build" ["with-profile" "+test" "do"
-      ["check-deps"]
-      ;["lint"]
-      ["test"]
-      ["compile"]
-      ["with-profile" "+ubercompile" "compile"]
+    "repl" ["do"
       ["clean"]
+      ["repl"]]
+    "ubercompile" ["with-profile" "+ubercompile" "compile"]
+    ;; Linting and tests
+    "check-vers" ["with-profile" "+lint" "ancient" "check" ":all"]
+    "check-jars" ["with-profile" "+lint" "do"
+      ["deps" ":tree"]
+      ["deps" ":plugin-tree"]]
+    "check-deps" ["do"
+      ["check-jars"]
+      ["check-vers"]]
+    "kibit" ["with-profile" "+lint" "do"
+      ["shell" "echo" "== Kibit =="]
+      ["kibit"]]
+    "eastwood" ["with-profile" "+lint" "eastwood" "{:namespaces [:source-paths]}"]
+    "lint" ["do"
+      ["kibit"]
+      ;["eastwood"]
+      ]
+    "ltest" ["with-profile" "+test" "ltest"]
+    ;; Build
+    "build" ^{:doc "Perform build steps."} ["do"
+      ["clean"]
+      ["ubercompile"]
+      ["check-vers"]
+      ;["lint"]
+      ["ltest"]
       ["uberjar"]]})
